a236f3803017f23415fb4231ea5c32aec606622c
juanb1926 <juanbejarano2619@gamil.com>
2023-11-28 08:32:36 -0500

    servicios

---
+++ src/app/servicios/compras.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  url = 'http://localhost/juan/src/app/compras/';
+
+  constructor(private http: HttpClient) { }
+
+  consultar() {
+    return this.http.get('${this.url}consulta.php');
+  }
+
+  insertar(articulo: any) {
+    return this.http.post('${this.url}insert.php', JSON.stringify(articulo));
+  }
+
+  eliminar(id: number) {
+    return this.http.get('${this.url}eliminar.php?id=${id}');
+  }
+
+  editar(datos: any) {
+    return this.http.post('${this.url}editar.php', JSON.stringify(datos));
+  }
+
+}
