8d994fecf7b7a06df081c7a5a1ad910853dfc9b5
juanb1926 <juanbejarano2619@gamil.com>
2024-01-10 10:34:51 -0500

    insetar_terminado

--- src/app/modulos/usuarios/usuarios.component.ts
+++ src/app/modulos/usuarios/usuarios.component.ts
@@ -18,13 +18,19 @@ export class Usuarioscomponent implements OnInit {
     usuario: "",
     clave: "",
     tipo: ""
-  } ;
-
+  };
+  // variables para validar
+  validNombre = true
+  validusuario = true
+  validclave = true
+  validtipo = true
 
   constructor(private suser: UsuariosService) { }
 
   ngOnInit(): void {
-    this.consulta(); } 
+    this.consulta();
+    this.limpiar();
+  }
 
 
   //mostrar formulario
@@ -38,6 +44,43 @@ export class Usuarioscomponent implements OnInit {
         break;
     }
   }
+  // limpiar campos
+  limpiar() {
+    this.user.Nombre = "";
+    this.user.usuario = "";
+    this.user.clave = "";
+    this.user.tipo = "";
+  }
+
+  // validar formulario
+  validar() {
+    if (this.user.Nombre == "") {
+      this.validNombre = false;
+    } else {
+      this.validNombre = true;
+    }
+
+
+    if (this.user.usuario == "") {
+      this.validusuario = false;
+    } else {
+      this.validusuario = true;
+    }
+
+
+    if (this.user.clave == "") {
+      this.validclave = false;
+    } else {
+      this.validclave = true;
+    }
+
+    if (this.user.tipo == "") {
+      this.validtipo = false;
+    } else {
+      this.validtipo = true;
+    }
+
+  }
 
 
   consulta() {
@@ -48,14 +91,20 @@ export class Usuarioscomponent implements OnInit {
   }
 
   ingresar() {
-    this.suser.insertar(this.user).subscribe((datos: any) => { 
-      if (datos['resultado']=='ok') {
-        this.consulta();
-      }
-    
-    })
-    this.mostrar(0);
+    this.validar();
+    if (this.validNombre == true && this.validusuario == true && this.validclave == true && this.validtipo == true) {
+      
+      this.suser.insertar(this.user).subscribe((datos: any) => {
+        if (datos['resultado'] == 'ok') {
+          // alerta datos
+          this.consulta();
+        }
+      })
+      this.mostrar(0);
+      this.limpiar();
+    }
+
   }
 
-  
+
 }
